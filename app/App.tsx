32e81e30c3e53bdc395195914ca7b3b931ef6b75
Andre Paul Cruz <andrepaulcruz29@gmail.com>
2025-11-02 22:12:19 -1000

    Initial Commit

---
+++ app/App.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>POLYNESIAN CULTURAL CENTER</Text>
+      <Text>Discover the Islands of Polynesia</Text>
+    </View>
+  );
+}
